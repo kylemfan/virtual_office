2551ea9ac5c57db462de07e7ad0f237a6f89e74b
Kyle Fan <43425389+kylemfan@users.noreply.github.com>
2024-07-18 21:12:31 -0700

    GameLoop.js updated

--- client/src/components/GameLoop.js
+++ client/src/components/GameLoop.js
@@ -25,6 +25,17 @@ const GameLoop = ({children, allCharactersData}) => {
         if (MOVE_DIRECTIONS[key]) {
             // ***********************************************
             // TODO: Add your move logic here
+            // 1. read current position
+            // 2. add delta (get from moveDirections to calculate new position)
+            // 3. update position (update function from mycharacter.js)
+
+            // if w pressed: currentPosition.y += -1;
+            // if a pressed: currentPosition.x += -1;
+            // if s pressed: currentPosition.y += 1;
+            // if d pressed: currentPosition.x += 1;
+            currentPosition.x += MOVE_DIRECTIONS.key[0];
+            currentPosition.y += MOVE_DIRECTIONS.key[1];
+
         }
     }, [mycharacterData]);
 
@@ -67,3 +78,5 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, {})(GameLoop);
+
+// states: 1. name 2. initial value 3. thing is updating
